bf58cc0426831eaea41a0f554515e61d8e15a3e3
shimoshimo-shimo <2CDN1112@mail.u-tokai.ac.jp>
2023-12-15 14:49:10 +0900

    demo2

--- game/Assets/script/typing2.cs
+++ game/Assets/script/typing2.cs
@@ -23,6 +23,8 @@ public class typing2 : MonoBehaviour
 
     private bool isTyping = false; // キーが押されているかどうかを示すフラグ
 
+    public int flag;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -41,34 +43,40 @@ public class typing2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         // キーボードの1, 2, 3が押されたら対応する_hatudouのランダムな要素を表示
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             DisplayRandomElement(_hatudou1);
+            flag = 1;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             DisplayRandomElement(_hatudou2);
+            flag = 2;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             DisplayRandomElement(_hatudou3);
+            flag = 3;
         }
 
         // m7Textに表示された文字列が正確に入力された場合、スライダーの値を10減少させる
         if (Input.GetKeyDown(KeyCode.Return)) // Enterキーが押された場合
         {
             CheckInputAndUpdateSlider();
+            isTyping = true;
             m8Text.text = "";
         }
 
         // キーが押されている場合、m8Textに入力された文字列を表示
         if (isTyping)
         {
+            
             foreach (KeyCode keyCode in Enum.GetValues(typeof(KeyCode)))
             {
                 // 数字キーは表示しない
-                if (keyCode >= KeyCode.Alpha0 && keyCode <= KeyCode.Alpha9)
+                if ((keyCode >= KeyCode.Alpha0 && keyCode <= KeyCode.Alpha9) || keyCode == KeyCode.Return)
                 {
                     continue;
                 }
@@ -86,11 +94,13 @@ public class typing2 : MonoBehaviour
     {
         if (array.Length > 0)
         {
+            m8Text.text = "";
             int randomIndex = UnityEngine.Random.Range(0, array.Length);
             // 最初の1文字以外を表示
             m7Text.text = array[randomIndex].Substring(1);
             // タイピング開始
             isTyping = true;
+            m8Text.text = "";
         }
         else
         {
@@ -104,7 +114,7 @@ public class typing2 : MonoBehaviour
         if (m8Text.text == m7Text.text)
         {
             // 入力が正確な場合、スライダーの値を10減少させる
-            mslider.value = Mathf.Max(0, mslider.value - 10);
+            mslider.value = Mathf.Max(0, mslider.value - 100);
 
             // 正解時の処理を呼び出し
             Correct();
@@ -117,6 +127,8 @@ public class typing2 : MonoBehaviour
 
         // タイピング終了
         isTyping = false;
+        m8Text.text = "";
+
 
         // スライダーが減少したら m8Text と m7Text をリセット
         if (mslider.value <= 0)
@@ -125,7 +137,7 @@ public class typing2 : MonoBehaviour
             m7Text.text = "";
 
             // 新しいシーンを読み込む（ここでは"GameOverScene"を仮定）
-            SceneManager.LoadScene("GameOverScene");
+            //SceneManager.LoadScene("GameOverScene");
         }
     }
 
@@ -135,6 +147,7 @@ public class typing2 : MonoBehaviour
         return Array.Exists(_hatudou1, element => element.Substring(1) == inputText) ||
                Array.Exists(_hatudou2, element => element.Substring(1) == inputText) ||
                Array.Exists(_hatudou3, element => element.Substring(1) == inputText);
+        m8Text.text = "";
     }
 
     // 正解時の処理
@@ -142,12 +155,40 @@ public class typing2 : MonoBehaviour
     {
         Debug.Log("正解");
         // ここに正解時の処理を追加
+        if (flag == 1)
+        {
+            DisplayRandomElement(_hatudou1);
+            m8Text.text = "";
+        }
+        else if (flag == 2)
+        {
+            DisplayRandomElement(_hatudou2);
+            m8Text.text = "";
+        }
+        else if (flag == 3)
+        {
+            DisplayRandomElement(_hatudou3);
+            m8Text.text = "";
+        }
+
     }
 
     // 不正解時の処理
     void Miss()
     {
         Debug.Log("不正解");
+        if (flag == 1)
+        {
+            DisplayRandomElement(_hatudou1);
+        }
+        else if (flag == 2)
+        {
+            DisplayRandomElement(_hatudou2);
+        }
+        else if (flag == 3)
+        {
+            DisplayRandomElement(_hatudou3);
+        }
         // ここに不正解時の処理を追加
     }
 }
