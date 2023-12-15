c843ac3f7eb060a5849fca7a3bf9b94caf454d68
shimoshimo-shimo <2CDN1112@mail.u-tokai.ac.jp>
2023-12-15 10:59:45 +0900

    demo

--- game/Assets/script/Inbattle.cs
+++ game/Assets/script/Inbattle.cs
@@ -43,7 +43,7 @@ public class Inbattle : MonoBehaviour
         objectToDeactivate.SetActive(false);
         pos.playerpos.Savepos();
         // シーンをロード
-        SceneManager.LoadScene("あ");
+        SceneManager.LoadScene("SampleScene");
     }
 }
 
