9ac134a3825600e0d8e6fd2e3b4dfa9ada76d1fe
shimoshimo-shimo <2CDN1112@mail.u-tokai.ac.jp>
2024-01-09 22:54:58 +0900

    0109

--- game/Assets/script/camera.cs
+++ game/Assets/script/camera.cs
@@ -21,7 +21,7 @@ public class camera : MonoBehaviour
         Vector3 cameraPos = target.transform.position; // cameraPosという変数を作り、追従する対象の位置を入れる
 
         // もし対象の横位置が0より小さい場合
-        if (target.transform.position.x < 0)
+        /*if (target.transform.position.x < 0)
         {
             cameraPos.x = 0; // カメラの横位置に0を入れる
         }
@@ -36,7 +36,7 @@ public class camera : MonoBehaviour
         if (target.transform.position.y > 0)
         {
             cameraPos.y = target.transform.position.y;   // カメラの縦位置に対象の位置を入れる
-        }
+        }*/
 
         cameraPos.z = -10; // カメラの奥行きの位置に-10を入れる
         Camera.main.gameObject.transform.position = cameraPos; //　カメラの位置に変数cameraPosの位置を入れる
