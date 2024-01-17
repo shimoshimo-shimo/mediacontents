8c3b809ceb192dbc8b414078cb87485bfafb82b4
shimoshimo-shimo <2CDN1112@mail.u-tokai.ac.jp>
2024-01-18 00:15:54 +0900

    0117 (2)

---
+++ game/Assets/script/GameEnd.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Update()
+    {
+        EndGame();
+    }
+
+    //ゲーム終了
+    private void EndGame()
+    {
+        //Escが押された時
+        if (Input.GetKey(KeyCode.Escape))
+        {
+
+            
+            UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+            Application.Quit();//ゲームプレイ終了
+        }
+
+    }
+}
---
+++ game/Assets/script/encount/Inbattle3.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Inbattle3 : MonoBehaviour
+{
+    public AudioClip sound1;
+    AudioSource audioSource;
+    public int j;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        j = PlayerPrefs.GetInt("J", 0);
+        if(j == 1)
+        {
+            objectToDeactivate.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        audioSource.PlayOneShot(sound1);
+        StartCoroutine(WaitAndLoadScene());
+    }
+
+    public GameObject objectToDeactivate;
+
+    IEnumerator WaitAndLoadScene()
+    {
+        // 座標保存
+        
+
+        // 非アクティブにしたいオブジェクトを DontDestroyOnLoad で指定
+        DontDestroyOnLoad(objectToDeactivate);
+
+        // 待機
+        yield return new WaitForSeconds(0.6f);
+        j = 1;
+        PlayerPrefs.SetInt("J", j);
+        // オブジェクトを非アクティブにする
+        objectToDeactivate.SetActive(false);
+        pos.playerpos.Savepos();
+        // シーンをロード
+        SceneManager.LoadScene("Typing Scene");
+    }
+}
+
+
+
